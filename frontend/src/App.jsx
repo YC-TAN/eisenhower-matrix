43acdb401f48b875a1e6e63cdd5b07c2679b9e7f
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-03-19 22:16:09 +1300

    Implement context provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from "react";
-import { v1 as uuid } from 'uuid'
-
 import MatrixPage from "./pages/MatrixPage";
-import { MOCK_TASKS } from "./utils/mockTasks";
+import { TaskProvider } from "./context/TaskContext";
 
 function App() {
-  const [tasks, setTasks] = useState(MOCK_TASKS);
-
-  const addTask = (taskObj) => {
-    const {task, important, urgent} = taskObj;
-    const newTask = {
-      id: uuid(),
-      task, 
-      urgent,
-      important,
-      status: 'pending',
-      createdAt: new Date().toISOString(),
-      completedAt: null
-    }
-    setTasks([...tasks, newTask]);
-  };
 
   return (
     <>
-      <div className="app-main">
-        <MatrixPage 
-          tasks={tasks}
-          onAdd={addTask}
-        />
-      </div>
+      <TaskProvider>
+        <div className="app-main">
+          <MatrixPage/>
+        </div>
+      </TaskProvider>
     </>
   );
 }
