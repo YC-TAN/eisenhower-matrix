86e4707cf79b98485635653a5608e1ac3f8587c0
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-03-26 20:52:54 +1300

    Feature(api): Implement errorHandler middleware

---
+++ backend/src/middleware/errorHandler.ts
@@ -0,0 +1,10 @@
+import { Response, Request, NextFunction } from "express";
+import {z} from 'zod';
+
+export const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => { 
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
