f1bfc63b22b91ca8eea83f4dd3056591035b5008
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-03-31 21:26:00 +1300

    feat(api): Add errorHandler for API errors

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,10 +1,71 @@
 import { Response, Request, NextFunction } from "express";
 import {z} from 'zod';
+import { logger, toError } from "src/utils/logger";
+import { NotFoundError } from "../utils/errors";
+import { ErrorResponse } from "src/types/api";
 
-export const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => { 
+// For standarc JS errors without HTTP codes
+const isNativeError = (error: unknown): error is { name: string; message: string } =>
+  typeof error === 'object' &&
+  error !== null &&
+  'name' in error &&
+  'message' in error;
+
+  // For API errors
+const hasStatusCode = (error: unknown): error is { statusCode: number; message: string } =>
+  typeof error === 'object' &&
+  error !== null &&
+  'statusCode' in error &&
+  'message' in error;
+
+export const errorMiddleware = (
+  error: unknown, 
+  _req: Request, 
+  res: Response<ErrorResponse>, 
+  next: NextFunction
+) => { 
+  logger.error('Unhandled error:', toError(error));
+  
   if (error instanceof z.ZodError) {
     res.status(400).send({ error: error.issues });
-  } else {
-    next(error);
+    return;
+  } 
+
+  if (error instanceof NotFoundError) {
+    res.status(404).json({ error: error.message });
+    return;
   }
+
+  if (hasStatusCode(error)) {
+    res.status(error.statusCode).json({ error: error.message });
+    return;
+  }
+
+  if (!isNativeError(error)) {
+    next(error);
+    return;
+  }
+
+  const err = toError(error);
+  
+  switch (err.name) {
+    case 'CastError':
+      res.status(400).json({ error: 'Malformatted id' });
+      return;
+    case 'ValidationError': // Mongo schema validation
+      res.status(400).json({ error: error.message });
+      return;
+    case 'MongoServerError':
+      if (error.message.includes('E11000')) {
+        res.status(409).json({ error: 'Duplicate key error' });
+        return;
+      }
+      break;
+    case 'SyntaxError':
+      res.status(400).json({ error: 'Invalid JSON' });
+      return;
+  }
+
+  logger.error("Final fallback error:", err.message);
+  res.status(500).json({ error: 'An unexpected error occurred.' });
 };
