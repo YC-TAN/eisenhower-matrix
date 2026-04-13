1f52666d774059eb024151bea8897a2c635eb765
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-14 09:52:27 +1200

    [Fix]: Fix typo a TaskProvider

--- frontend/testSetup.js
+++ frontend/testSetup.js
@@ -2,7 +2,7 @@ import { beforeEach, vi, afterEach } from 'vitest'
 import { cleanup, render } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 
-import { TasksProvider } from './src/context/TaskContext'
+import { TaskProvider } from './src/context/TaskContext'
 
 beforeEach(() => {
   localStorage.clear();
@@ -13,9 +13,9 @@ afterEach(() => {
   cleanup()
 })
 
+// For integration test
 const customRender = (ui, options) =>
-  render(ui, { wrapper: TasksProvider, ...options });
+  render(ui, { wrapper: TaskProvider, ...options });
 
-// 3. Export everything from RTL + our custom render
 export * from '@testing-library/react';
 export { customRender as render };
